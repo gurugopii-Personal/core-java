ef1830891473b1dc4ecef7f4b256dbb0679f10ce
Gopinath Gurunathan <gopinath.gurunathan@setel.my>
2021-06-05 15:56:55 +0800

    calcultor class for arthmetic operation

---
+++ core-java-training/src/Calculator.java
@@ -0,0 +1,14 @@
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		System.out.println("Arthmetic operations");
+		System.out.println("Addition is " + (a+b));
+		System.out.println("Subraction is " + (a-b));
+		System.out.println("Multiplication is " + (a*b));
+		System.out.println("Division is " + (a/b));
+	}
+
+}
